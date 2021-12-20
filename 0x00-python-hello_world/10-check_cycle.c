33ae0a04675a944c535da4b96ea69c4dc40c9a15
root <flipeprez@gmail.com>
2021-12-20 20:56:41 +0000

    ex 10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -8,7 +8,7 @@ int check_cycle(listint_t *list)
 	if(!list)
 	return (0);
 	
-	while(rapido->next->next)
+	while(rapido -> next && rapido->next->next)
 	{
 		rapido = rapido -> next -> next;
 		lento = lento -> next;
