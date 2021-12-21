8cca923f9ef2864874466e5f8c7be26daacd35b8
flipeprez <flipeprez@gmail.com>
2021-12-21 14:51:06 -0800

    ex 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+    print("{}".format(chr(ord (i)) -32)
+            if ord (i) >= ord ("a") and
+            ord (i) <= ord("z")) else i), end="")
+    print()
