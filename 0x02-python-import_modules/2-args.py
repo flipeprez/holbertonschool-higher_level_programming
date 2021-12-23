763b8fb73d3777a5a9e3f727d60f68c3c2f1da0a
flipeprez <flipeprez@gmail.com>
2021-12-23 09:10:14 -0800

    ex 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+from sys import argv
+
+
+def argument():
+    arg_str = "arguments"
+    ifna = "."
+    length = len(argv) - 1
+    if (length == 1):
+        arg_str = "argument"
+    if (length - 1 >= 0):
+        ifna = ":"
+    print("{} {}{}".format(length, arg_str, ifna))
+
+    for index, arg in enumerate(argv):
+        if (index > 0):
+            print("{} : {}".format(index, arg))
+
+
+if __name__ == "__main__":
+    argument()
