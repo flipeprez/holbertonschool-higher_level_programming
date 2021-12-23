e7c2fc9d486d6dc280b34783bdbcfc4386768e73
flipeprez <flipeprez@gmail.com>
2021-12-23 10:22:51 -0800

    ex 3 change the paradigm

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
-from sys import argv
+if __name__ == "__main__":
+    import sys
 
-
-def add_all():
-    sum = 0
-
-    for i, number in enumerate(argv):
-        if (i > 0):
-            sum = sum + int(number)
-    print("{:d}".format(sum))
-
-    if __name__ == "__main__":
-        add_all()
+    number = 0
+    for i in range(1, len(sys.argv)):
+        number = number + int(sys.argv[i])
+print(number)
