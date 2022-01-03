31128653f798d23108d35b6405a2b93455b13b08
flipeprez <flipeprez@gmail.com>
2022-01-03 13:51:45 -0800

    max without max and set with very small value

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     bigger = -999999
+    if len(my_list) == 0:
+        return (None)
     for i in my_list:
         if i > bigger:
             bigger = i
     return (bigger)
-    if len(my_list) == 0:
-        return (None)
