8210e8b67a7a3f22b592260a1e911cc483e7dd2f
flipeprez <flipeprez@gmail.com>
2022-01-03 07:29:29 -0800

    fix function

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    for i in my_list:
-        if idx < 0:
-            return (None)
-        if idx > len(my_list):
-            return (None)
+    if idx < 0:
+        return (None)
+    if idx > len(my_list) - 1:
+        return (None)
 
 
-    return(my_list)
+    return my_list[idx]
