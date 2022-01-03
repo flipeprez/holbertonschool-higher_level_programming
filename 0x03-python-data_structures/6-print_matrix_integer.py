ddc5a031965de604d47c5252ddd7f70f29701200
flipeprez <flipeprez@gmail.com>
2022-01-03 11:28:07 -0800

    matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,6 +5,6 @@ def print_matrix_integer(matrix=[[]]):
         for j in i:
             if var != 0:
                 print(" ", end='')
-            print ("{:d}".format(j), end='')
+            print("{:d}".format(j), end='')
             var += 1
         print()
