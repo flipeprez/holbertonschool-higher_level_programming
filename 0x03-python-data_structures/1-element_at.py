ce38c9105fd65df0b611ac138a0c4848f2ccc949
flipeprez <flipeprez@gmail.com>
2022-01-03 07:08:40 -0800

    variable fixed

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    for i in my_list:
+    for idx in my_list:
         if idx < 0:
             return (None)
         if idx > my_list.append(idx):
