10cbcac4baaa86fe168afbe69b559c14b316bf7d
flipeprez <flipeprez@gmail.com>
2022-01-04 08:50:27 -0800

    common elements

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    a = set(set_1)
-    b = set(set_2)
-
-    if len(a.intersection(b)) > 0:
-        return(a.intersection(b))
+    return (set_1 & set_2)
