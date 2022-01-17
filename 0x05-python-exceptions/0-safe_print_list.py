67da45f6d6d7536c4a3cc0bfdb47a6255777eac6
flipeprez <flipeprez@gmail.com>
2022-01-17 10:57:12 -0800

    fixing whitespaces

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         for item in range(0, x):
             print("{}".format(my_list[item]), end="")
-            indx +=1
+            indx += 1
     except IndexError:
         pass
     print()
