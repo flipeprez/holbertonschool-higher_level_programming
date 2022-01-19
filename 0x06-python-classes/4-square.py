ef5db47c5ae9d148d9945e03e24db05a20d2911e
flipeprez <flipeprez@gmail.com>
2022-01-18 16:14:09 -0800

    fix comments

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -12,12 +12,12 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
-    
+
     @property
     def size(self):
         '''lala'''
         return self.__size
-    
+
     @size.setter
     def size(self, value):
         '''lala'''
@@ -27,7 +27,7 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
-    
+
     def area(self):
         '''lala'''
         return self.__size * self.__size
