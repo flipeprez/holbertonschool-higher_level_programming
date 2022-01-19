940e2279feedc1ccc846cc8f7e925c1ef6a63bf7
flipeprez <flipeprez@gmail.com>
2022-01-18 16:09:23 -0800

    fix comments

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-class Square:
+'''lalala'''
 
+
+class Square:
+    '''lalal'''
     def __init__(self, size=0,):
+        '''lalal'''
         if (type(size) != int):
             raise TypeError("size must be an integer")
         if (size < 0):
@@ -9,4 +13,5 @@ class Square:
         self.__size = size
 
     def area(self):
+        '''lala'''
         return self.__size * self.__size
