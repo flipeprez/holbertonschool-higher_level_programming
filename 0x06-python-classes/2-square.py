6bdb62d531ed592176913fec9d25af20d2810571
flipeprez <flipeprez@gmail.com>
2022-01-18 16:07:52 -0800

    fix comments

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-'''lalala'''
+'''func'''
 
 
 class Square:
-'''lalala'''
+    '''class comment'''
     def __init__(self, size=0):
-        '''lalala'''
+        '''def func'''
         if (type(size) != int):
             raise TypeError("size must be an integer")
         if (size < 0):
