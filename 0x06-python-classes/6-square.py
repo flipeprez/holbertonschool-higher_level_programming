f1858920f78cb274a61f99d0e8e6b4e2d84c3ba1
flipeprez <flipeprez@gmail.com>
2022-01-19 16:40:30 -0800

    completing functions

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,60 +1,65 @@
 #!/usr/bin/python3
-    '''lala'''
+'''lala'''
 
 
 class Square:
     '''lala'''
     def __init__(self, size=0, position=(0, 0)):
         '''lala'''
-	    if type(size) is not int:
-		raise TypeError("size must be an integer")
-	    elif size < 0:
-		raise ValueError("size must be >= 0")
-	    else:
-		self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
 
     @property
     def size(self):
-	'''lalal'''
-	return self.__size
+        '''lalal'''
+        return self.__size
 
     @size.setter
     def size(self, value):
-	'''lalal'''
-	if type(value) is not int:
-    	    raise TypeError("size must be an integer")
-	elif value < 0:
-	    raise ValueError("size must be >= 0")
-	else:
-	    self.__size = value
+        '''lalal'''
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
 
     def area(self):
-	'''lalala'''
-	return self.__size * self.__size
+        '''lalala'''
+        return self.__size * self.__size
 
     def my_print(self):
-	'''lalal'''
-	if self.__size == 0:
-	    print()
-	elif self.__size > 0:
-	    for a in range(0, self.__size):
-		for h in range(0, self.__size):
-		    print("#", end="")
-		print()
-        
+        '''lalal'''
+        if self.__size == 0:
+            print()
+        elif self.__size > 0:
+            for a in range(0, self.__size):
+                for h in range(0, self.__size):
+                    print("#", end="")
+                print()
+
     @property
     def position(self):
         '''lalal'''
         return self.__position
-        
+
     @position.setter
     def position(self, value):
         '''laala'''
-        raise TypeError("position must be a tuple of 2 positive intergers")
-
-    def my_print(self):
-        '''lalala'''
-        if self.__size == 0:
-            print()
-            return
-
+        if value is None:
+            self.__position = (0, 0)
+            raise TypeError("position must be a tuple of 2 positive intergers")
+        if type(value) != tuple or len(value) != 2:
+            self.__position = (0, 0)
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if type(value[0]) != int or type(value[1]) != int:
+            self.__position = (0, 0)
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if value[0] < 0 or value[1] < 0:
+            self.__position = (0, 0)
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
