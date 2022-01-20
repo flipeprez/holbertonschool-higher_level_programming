a391f7521826c295bb7bb096581bbeeb7d17a4c7
flipeprez <flipeprez@gmail.com>
2022-01-19 17:00:07 -0800

    more fixing functions

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -51,8 +51,8 @@ class Square:
     def position(self, value):
         '''laala'''
         if isinstance(value, tuple) and len(value) == 2:
-            if (value[0],int) and isinstance(value[1], int):
+            if (value[0], int) and isinstance(value[1], int):
                 if value[0] >= 0 and value[1] >= 0:
                     self.__position = value
                     return
-                raise TypeError("position must be a tuple of 2 positive integers")
+        raise TypeError("position must be a tuple of 2 positive integers")
