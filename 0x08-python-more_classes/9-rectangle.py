d4bcdb02de8cf845a56d6be5947886e17b32a7b2
flipeprez <flipeprez@gmail.com>
2022-01-24 15:02:23 -0800

    fixing outputs

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -9,14 +9,14 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         '''def func'''
-        if type(value) is not int:
+        if type(height) is not int:
             raise TypeError("height must be an integer")
-        if value < 0:
+        if height < 0:
             raise ValueError("height must be >= 0")
-        if type(value) is not int:
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
         self.height = height
         self.width = width
         number_of_instance = 0
