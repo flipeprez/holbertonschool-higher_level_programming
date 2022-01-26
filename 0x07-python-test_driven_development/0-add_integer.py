17b37f49990d69a870fef474424abad16335d8ab
flipeprez <flipeprez@gmail.com>
2022-01-26 08:50:42 -0800

    fixing code msg error

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -5,7 +5,7 @@
 def add_integer(a, b=98):
     '''add integer function'''
     if not isinstance(a, int) and not isinstance(a, float):
-        raise TypeError("a must be a integer")
+        raise TypeError("a must be an integer")
     if not isinstance(b, int) and not isinstance(b, float):
-        raise TypeError("b must be a integer")
+        raise TypeError("b must be an integer")
     return int(a + b)
