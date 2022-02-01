294cb6fe0b6688b19775d9224ae5ca9642b460dc
flipeprez <flipeprez@gmail.com>
2022-01-31 17:27:02 -0800

    modify function again and add more comments

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+'''unbelievable'''
+
+
 def is_same_class(obj, a_class):
     '''same class'''
-    return type(obj) is a_class
+    if type(obj) is a_class:
+        return True
+    return False
