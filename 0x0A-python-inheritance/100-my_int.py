51fccbc6b70bd2a92a65b9244f67e7f04d8cc5f3
flipeprez <flipeprez@gmail.com>
2022-01-31 18:53:09 -0800

    fix indentation and spaces

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3
 '''def class'''
 
+
 class Myint(int):
     '''coment'''
+
     def __init__(self, number):
         '''comment'''
         self.number = number
+
     def __ne__(self, val):
         '''comment'''
         return (self.number == val)
+
     def __eq__(self, val):
         '''comment'''
         return (self.number != val)
+
     def __str__(self):
         '''comment'''
         return (str(self.number))
