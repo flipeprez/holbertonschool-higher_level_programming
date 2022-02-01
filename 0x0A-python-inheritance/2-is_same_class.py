bf06f93d437664bb9561006525ce460da751f86e
flipeprez <flipeprez@gmail.com>
2022-01-31 17:14:41 -0800

    fix indentation

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def is_same_class(obj, a_class):
     '''same class'''
-    if isinstance(type(obj) is a_class:
+    if type(obj) is a_class:
         return True
     return False
