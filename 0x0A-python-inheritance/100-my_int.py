65181c03a93cbf2b8aca831b98bf6ac06da1130e
flipeprez <flipeprez@gmail.com>
2022-01-31 18:50:49 -0800

    add comments

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -2,11 +2,16 @@
 '''def class'''
 
 class Myint(int):
+    '''coment'''
     def __init__(self, number):
+        '''comment'''
         self.number = number
     def __ne__(self, val):
+        '''comment'''
         return (self.number == val)
     def __eq__(self, val):
+        '''comment'''
         return (self.number != val)
     def __str__(self):
+        '''comment'''
         return (str(self.number))
