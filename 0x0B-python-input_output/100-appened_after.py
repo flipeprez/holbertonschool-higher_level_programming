7b2232afa188e2d10adb1f3decbd24bfdde63f58
flipeprez <flipeprez@gmail.com>
2022-02-01 18:32:47 -0800

    advanced

---
+++ 0x0B-python-input_output/100-appened_after.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''comments'''
+
+
+def appened_after(filename="", search_string="", new_string=""):
+    '''comment'''
+    with open(filename) as f:
+        aux = ""
+        for line in f:
+            aux += linea
+            if search_string in linea:
+                aux += new_string
+        f.seek(0)
+        f.write(aux)
