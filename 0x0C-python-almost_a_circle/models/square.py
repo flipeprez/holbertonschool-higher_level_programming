178a9341a727de97507cf808629ba9ae8a66ba8b
flipeprez <flipeprez@gmail.com>
2022-02-07 09:11:50 -0800

    change error size

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -40,7 +40,7 @@ class Square(Rectangle):
     def size(self, size):
         '''def size setter'''
         if type(size) is not int:
-            raise TypeError("width must be an integer")
+            raise TypeError("size must be an integer")
         if size <= 0:
             raise ValueError("size must be > 0")
         else:
