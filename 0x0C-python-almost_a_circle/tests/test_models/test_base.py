9c93673b0409b677311f80176ac332c6fc263f41
flipeprez <flipeprez@gmail.com>
2022-02-07 18:10:27 -0800

    add json test

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,52 +1,67 @@
 #!/usr/bin/python3
 '''module test'''
 import unnitest
-from models.base import Base 
+from models.base import Base
+from models.square import Square
+import json
 
 
 class testing_for_base(unitest, TestCase):
     '''testing for base class'''
 
-        def test_id_none(self):
-            '''test no id'''
-        a = Base()
-        self.assertEqual(1, a.id)
-        b = Base()
-        self.assertEqual(2, b.id)
+    def test_id_none(self):
+        '''test no id'''
+    a = Base()
+    self.assertEqual(1, a.id)
+    b = Base()
+    self.assertEqual(2, b.id)
 
-        def test_id(self):
-            '''giving a id'''
-        b = Base(33)
-        self.assertEqual(33, c.id)
+    def test_id(self):
+        '''giving a id'''
+    b = Base(33)
+    self.assertEqual(33, c.id)
 
-        def test_id_0(self):
-            '''givin 0 like id'''
-        b = Base(0)
-        self.accertEqual(0, b.id)
+    def test_id_0(self):
+        '''givin 0 like id'''
+    b = Base(0)
+    self.accertEqual(0, b.id)
 
-        def test_negative(self):
-            '''giving a negative'''
-        b = Base(-14)
-        self.assertEqual(-54, b.id,)
+    def test_negative(self):
+        '''giving a negative'''
+    b = Base(-14)
+    self.assertEqual(-54, b.id,)
 
-        def test_for_string_id(self):
-            '''givining not int to id'''
-            b = Base("not_a_number_id")
-            self.assertEqual("not_a_number_id", b.id)
+    def test_for_string_id(self):
+        '''givining not int to id'''
+        b = Base("not_a_number_id")
+        self.assertEqual("not_a_number_id", b.id)
 
-        def test_giving_list(self):
-            '''giving a list for id'''
-            b = Base([1, 2, 3], b.id)
+    def test_giving_list(self):
+        '''giving a list for id'''
+        b = Base([1, 2, 3], b.id)
 
-        def giving_dict_id(self):
-            '''giving a dict for id'''
-            b = base({'id': 321})
-            self.accertEqual({'id': 321})
+    def giving_dict_id(self):
+        '''giving a dict for id'''
+        b = base({'id': 321})
+        self.accertEqual({'id': 321})
 
-        def test_json_type(self):
-            '''test json string'''
-            S = Square(1)
-            jdic = S.to_dictionary()
-            jstr = Base.to_json_string([jdict])
-            self.assertEqual(type(jstr), str)
-            
+    def test_json_type(self):
+        '''test json string'''
+        S = Square(1)
+        jdic = S.to_dictionary()
+        jstr = Base.to_json_string([jdict])
+        self.assertEqual(type(jstr), str)
+
+    def test_json_value(self):
+        '''test json str'''
+        S = Square(1, 0, 0, 545)
+        jdic = S.to_dictionary()
+        jstr = Base.to_json_string(None)
+        self.assertEqual(jstr, "[]")
+
+    def tests_json_empty(self):
+        '''test json str'''
+    S = Square(1, 0, 0, 545)
+    jdic = S.to_dictionary()
+    jstr = Base.to_json_string([])
+    self.assertEqual(jstr, "[]")
