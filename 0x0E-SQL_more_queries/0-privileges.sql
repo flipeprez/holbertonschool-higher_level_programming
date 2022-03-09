d5f98724ad4172f8552ed29f7b877b0dbcf6a9b5
flipeprez <flipeprez@gmail.com>
2022-03-09 10:39:08 -0800

    add first 3 ex

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,3 @@
+-- script that list all privileges of the mysql users
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,2 @@
+-- write a script that creates the MYSQL server user user_0d_1
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd'
