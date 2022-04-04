4e84166a1a8b80bfc61323def018c3874eab18ab
flipeprez <flipeprez@gmail.com>
2022-04-04 10:24:57 -0700

    python to uppercase

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const varForc = 'C is fun';
-const varForpy = 'python is cool';
+const varForpy = 'Python is cool';
 const varForjs = 'JavaScript is amazing';
 console.log(varForc);
 console.log(varForpy);
