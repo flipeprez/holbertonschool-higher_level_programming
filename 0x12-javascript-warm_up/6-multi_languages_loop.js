bb353c145d1a1f944af88b1352b722d10362eea2
flipeprez <flipeprez@gmail.com>
2022-04-04 14:12:04 -0700

    fix indent

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
-const list = ['C is fun', 'Python is cool', 'JavaScript is amazing' ];
+const list = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (const property in list) {
-console.log(`${list[property]}`);
+  console.log(`${list[property]}`);
 }
-
-//console.log(varForc);
-//console.log(varForpy);
-//console.log(varForjs);
