dc2666ee0dc92ed55253353d010cbdbf6d152493
flipeprez <flipeprez@gmail.com>
2022-04-04 10:03:02 -0700

    first commit ex 0 and 1

---
+++ 0x12-javascript-warm_up/1-multi_languajes.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const varForc = 'C is fun';
+const varForpy = 'python is cool';
+const varForjs = 'JavaScript is amazing';
+console.log(varForc);
+console.log(varForpy);
+console.log(varForjs);
