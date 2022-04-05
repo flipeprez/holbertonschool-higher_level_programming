89dbfdbbcb34d8b7becdb6c759161a7798f9a4a0
flipeprez <flipeprez@gmail.com>
2022-04-04 17:45:38 -0700

    fix indent

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-let list = process.argv[2];
+const list = process.argv[2];
 
 if (!isNaN(parseInt(process.argv[2]))) {
   for (let i = 0; i < list; i++) {
-  console.log('C is fun');
+    console.log('C is fun');
   }
 } else {
   console.log('Missing number of occurrences');
