58d25e5f39bed40e7e3ab9401b50b264ed2ddec7
flipeprez <flipeprez@gmail.com>
2022-04-05 14:10:33 -0700

    add ex for checker

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+class Square extends Rectangle {
+  constructor(size) {
+    super(size, size);
+  }
+  charPrint(c) {
+  if (typeof (c) === 'undefined') {
+    c = 'X';
+  }
+  for (let i = 0; i < this.height; i++) {
+    let i = '';
+    for (let k = i; k < this.width; k++) {
+      i = i + c;
+    }
+    console.log(i); 
+  }
+  }
+}
+module.exports = Square;
+
