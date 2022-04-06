0712a49997ec5cbcf9538a50af225dcaa6351b6a
flipeprez <flipeprez@gmail.com>
2022-04-05 19:24:42 -0700

    fix requierements errors

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  let newList = [];
+  const newList = [];
   for (let i = list.length - 1; i > -1; i--) {
     newList.push(list[i]);
   }
   return newList;
 };
-
