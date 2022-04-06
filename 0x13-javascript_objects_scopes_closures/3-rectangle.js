fa6305770c6ff7faf3da7b7559f426a3fe27a5a1
flipeprez <flipeprez@gmail.com>
2022-04-05 19:17:48 -0700

    fix requierements errors

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,20 +1,20 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor(w , h){
-  if (w > 0 && h > 0) {
-    this.width = w;
-    this.height = h;
-  }
-  }
-  print() {
-  for (let i = 0; i < this.height; i++) {
-    let i = '';
-    for (let k = 0; k < this.width; k++) {
-      i = i + 'X';
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
-    console.log(i);
   }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let i = '';
+      for (let k = 0; k < this.width; k++) {
+        i = i + 'X';
+      }
+      console.log(i);
+    }
   }
 }
 module.exports = Rectangle;
-
