449bc4530dacf3702bdc4d21d7a8e282e79a5c86
flipeprez <flipeprez@gmail.com>
2022-04-10 19:42:41 -0700

    fix function

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,28 +3,27 @@ import sys
 import MySQLdb
 
 
-def GetAllStates(username, password, my_db):
+def get_all_states(username, password, db_name):
     '''
-    list  states
+        List all the states in the given database
     '''
     db = MySQLdb.connect(host="localhost",
                          user=username,
                          passwd=password,
-                         db=my_db,
+                         db=db_name,
                          port=3306)
 
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM `states` ORDER BY id")
+    cursor.execute("SELECT * FROM `states` ORDER BY id ASC")
     rows = cursor.fetchall()
     for row in rows:
         print(row)
     cursor.close()
     db.close()
 
-
 if __name__ == "__main__":
-    fsysargv0 = sys.argv
+    credentials = sys.argv
     username = sys.argv[1]
-    password = sys.argv[2]
-    my_db = sys.argv[3]
-    GetAllStates(username, password, my_db)
+    passwd = sys.argv[2]
+    db_name = sys.argv[3]
+    get_all_states(username, passwd, db_name)
