3257c16ec7614528ba2652d8d814d3c9996d0423
flipeprez <flipeprez@gmail.com>
2022-04-27 22:04:16 -0700

    add ex 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''comment'''
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    if len(sys.argv) == 2:
+        letter = sys.argv[1]
+    else:
+        letter = ""
+    payload = {"q": letter}
+    response = requests.post(url, payload)
+    try:
+        user = response.json()
+        if (len(user) < 1):
+            print("No result")
+        else:
+            print("[{}] {}".format(user["id"], user["name"]))
+    except:
+        print("Not a valid JSON")
