b10cb48a8a9beb44187da5f1b36ccea92ad7d051
flipeprez <flipeprez@gmail.com>
2022-04-27 22:09:16 -0700

    add ex 10

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''comments'''
+import sys
+import requests
+
+if __name__ == "__main__":
+    user_name = sys.argv[1]
+    passwd = sys.argv[2]
+    url = "https://api.github.com/user"
+    response = requests.get(url, auth=(user_name, passwd))
+    try:
+        print(response.json()["id"])
+    except KeyError:
+        print("None")
