b6baa9ea6269365d1dd4f943af0610804c3aa3bc
flipeprez <flipeprez@gmail.com>
2022-05-18 01:05:51 +0000

    fix semistandard

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
-let request = require('request');
-let url = process.argv[2];
+const request = require('request');
+const url = process.argv[2];
 
 request(url, function (err, response, body) {
   if (err) {
     console.log(err);
   } else {
-    console.log('code: ' + response['statusCode']);
+    console.log('code: ' + response.statusCode);
   }
 });
