2dfd56f38689cc273ec1fc136989571b73d50295
flipeprez <flipeprez@gmail.com>
2022-05-18 00:55:51 +0000

    add ex with correct output

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const process = require('process');
+
+fs.readFile(process.argv[2], 'utf8', (err, data) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(data);
+});
---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+let request = require('request');
+let episode = process.argv[2];
+let url = 'http://swapi-api.hbtn.io/api/films/' + episode;
+
+request(url, function (err, data, body) {
+  if (err) throw err;
+  console.log(JSON.parse(body)['title']);
+});
