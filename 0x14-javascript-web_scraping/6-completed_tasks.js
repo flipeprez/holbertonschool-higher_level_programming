dda39e476661517513a487bbc495a4254d311e17
flipeprez <flipeprez@gmail.com>
2022-05-18 02:08:29 +0000

    add ex need to fix output

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+let request = require('request');
+let url = process.argv[2];
+let myDict = {};
+
+request(url, function (err, data, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    let response = JSON.parse(body);
+
+    for (let i = 0; i < response.length; i++) {
+      if (response[i]['completed'] === true) {
+        myDict[response[i]['userId']] = 1;
+        } else {
+          myDict[response[i]['userId']] += 1;
+        }
+      }
+    }
+  console.log(myDict);
+});
